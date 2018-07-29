ea9644112c83c7ae773a0dc43e54869e5823c439
bgroveben <bgroveben@gmail.com>
2018-07-29 04:00:54 -0700

    Finish timing decorator for comparing function runtimes

--- NevadaDashboard/timer.py
+++ NevadaDashboard/timer.py
@@ -27,7 +27,41 @@ def timeit(_func=None, *, repeat=3, number=1000, file=sys.stdout):
     ...
     """
 
-_repeat = functools.partial(itertools.repeat, None)
+    _repeat = functools.partial(itertools.repeat, None)
 
-def wrap(func):
-    pass
+    def wrap(func):
+        @functools.wraps(func)
+
+    def _timeit(*args, **kwargs):
+        # Suspend garbage collection so that times can be compared.
+        gcold = gc.isenabled()
+        gc.disable()
+
+        try:
+            trials = []
+            for _ in _repeat(number):
+                start = _timer()
+                result = func(*args, **kwargs)
+                end = _timer()
+                total += end - start
+            trials.append(total)
+
+            # Now we are looking for the average time from the best trial according to Pythons timeit module.
+            best = min(trials) / number
+            print('Best of {} trials with {} function'
+                  ' calls per trial: '.format(repeat, number))
+            print('Function `{}` ran in average'
+                  ' of {:0.3f} seconds.'.format(func.__name__, best),
+                  end='\n\n', file=file)
+
+        finally:
+            if gcold:
+                gc.enable()
+        # Result is returned only once
+        return result
+    return _timeit
+# Syntax trick from Python's @dataclass
+if _func is None:
+    return wrap
+else:
+    return wrap(_func)
